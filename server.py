3f262a86de2ab98349927123ab5439995c730dc3
Sabreen Mehvish Mohammed <sabreen.mohammed@live.com>
2023-11-28 01:03:19 -0800

    isolating the server test

--- server.py
+++ server.py
@@ -1,25 +1,25 @@
 from flask import Flask, request, jsonify
-from constrainedDiffusor import diffusionProcessorDispatch
+# from constrainedDiffusor import diffusionProcessorDispatch
 
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     return jsonify(['Hello World!'])
 
-@app.route('/process', methods=['POST'])
-def process_image():
-    try:
-        # Extract Image and Parameters from the request
-        controlnet_type = request.files['controlnetType']
-        params = request.json['controlnetParameters']  # Assuming parameters are sent as JSON
-
-        # Call the main function from your script
-        response = diffusionProcessorDispatch.main(image, params)
-
-        # Return the response
-        return jsonify(response)
-    except Exception as e:
-        return jsonify({"error": str(e)})
+# @app.route('/process', methods=['POST'])
+# def process_image():
+#     try:
+#         # Extract Image and Parameters from the request
+#         controlnet_type = request.files['controlnetType']
+#         params = request.json['controlnetParameters']  # Assuming parameters are sent as JSON
+#
+#         # Call the main function from your script
+#         response = diffusionProcessorDispatch.main(image, params)
+#
+#         # Return the response
+#         return jsonify(response)
+#     except Exception as e:
+#         return jsonify({"error": str(e)})
 
 
 if __name__ == '__main__':
